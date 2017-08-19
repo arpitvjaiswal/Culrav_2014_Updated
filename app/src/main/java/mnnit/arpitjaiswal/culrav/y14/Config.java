0b21c0e76a754ed73dc97bef09f4dfcba3338d6a
Arpit Jaiswal <arpit.jaiswal@oracle.com>
2017-08-19 19:59:20 +0530

    Add facebook link

---
+++ app/src/main/java/mnnit/arpitjaiswal/culrav/y14/Config.java
@@ -0,0 +1,11 @@
+package mnnit.arpitjaiswal.culrav.y14;
+
+/**
+ * External Links, constants, and functions for other classes
+ */
+
+final class Config {
+
+    final static String FB_PAGE_CULRAV = "http://www.facebook.com/mnnit.culrav";
+    final static String FB_PAGE_CULARV_ID = "200290116782899";
+}
--- app/src/main/java/mnnit/arpitjaiswal/culrav/y14/HomeActivity.java
+++ app/src/main/java/mnnit/arpitjaiswal/culrav/y14/HomeActivity.java
@@ -1,6 +1,11 @@
 package mnnit.arpitjaiswal.culrav.y14;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -9,6 +14,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -64,7 +71,7 @@ public class HomeActivity extends AppCompatActivity
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
@@ -80,11 +87,36 @@ public class HomeActivity extends AppCompatActivity
             case R.id.nav_web:
                 break;
             case R.id.nav_fb:
+                openFacebookPage();
                 break;
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    /**
+     * Open facebook page of Culrav
+     * <p>
+     * First look if the user has facebook app installed
+     * If true, open the page inside the app, otherwise provide
+     * option to open the link with preferred app/browser
+     */
+    private void openFacebookPage() {
+        final String facebookUri = "fb://page/" + Config.FB_PAGE_CULARV_ID;
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(facebookUri));
+
+        final PackageManager packageManager = getPackageManager();
+        List<ResolveInfo> list = packageManager.queryIntentActivities(intent,
+                PackageManager.MATCH_DEFAULT_ONLY);
+
+        if (list.size() > 0) {
+            startActivity(intent);
+        } else {
+            intent.setData(Uri.parse(Config.FB_PAGE_CULRAV));
+            startActivity(Intent.createChooser(intent, getString(R.string.misc_load_with)));
+        }
     }
 }
